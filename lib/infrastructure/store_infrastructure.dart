ec79b30d12f98557bac49ce29e8dcb95d763d1bc
mamunhossain <mh72156@gmail.com>
2023-03-25 12:34:53 +0600

    data infrastructure implementation

--- lib/infrastructure/store_infrastructure.dart
+++ lib/infrastructure/store_infrastructure.dart
@@ -1,13 +1,21 @@
+import 'package:online_attendance_app/core/api_service.dart';
+import 'package:online_attendance_app/core/toast.dart';
 import 'package:online_attendance_app/domain/store.dart';
 import 'package:online_attendance_app/domain/store_domain.dart';
 
-class StoreInfrastructure extends StoreDomain{
+class StoreInfrastructure extends StoreDomain {
+  final ApiService apiService;
+
+  StoreInfrastructure({required this.apiService});
 
   @override
-  Future<List<Store>> getStores({page = 1}) {
-
+  Future<List<Store>> getStores({page = 1}) async {
+    try {
+      final stores = await apiService.getAllStoreData(page: page);
+      return stores;
+    } catch (e) {
+      ToastMessage.error('error accured => $e');
+    }
+    return [];
   }
-
-
-
 }
